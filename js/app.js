2203cae85606a327584d65af26b99324a226429e
Alejo1492 <Alejoudec92@gmail.com>
2018-08-02 03:47:33 -0500

    Obtencion de posiciones en el tablero

--- js/app.js
+++ js/app.js
@@ -18,7 +18,45 @@ function tituloParpadeo(elemento){
         blanco(elemento);
       });}
 }
+//Numeros aleatorios
+function numerosAleatorios(numero1,numero2){
+  numero1 = Math.ceil(numero1);
+  numero2 = Math.floor(numero2);
+  return Math.floor(Math.random()*(numero2 - numero1))+numero1;
+}
+//Filas y columnas del tablero
+function obtenerPosicion(tipo, pos){
+  var col1 = $(".col-1").children();
+  var col2 = $(".col-2").children();
+  var col3 = $(".col-3").children();
+  var col4 = $(".col-4").children();
+  var col5 = $(".col-5").children();
+  var col6 = $(".col-6").children();
+  var col7 = $(".col-7").children();
 
+  var columnas = $([col1,col2,col3,col4,col5,col6,col7]);
+  if(typeof pos === "number"){
+    var filas = $([col1.eq(pos),col2.eq(pos),col3.eq(pos),col4.eq(pos),
+      col5.eq(pos),col6.eq(pos),col7.eq(pos)]);
+  }else{
+    pos="";
+  }
+  if(tipo === "columna"){
+    return columnas;
+  }else if(tipo === "fila" && pos !== ""){
+    return filas;
+  }
+}
+//filas
+function filas(pos){
+  var fila = obtenerPosicion("fila",pos);
+  return fila;
+}
+//columnas
+function columnas(pos){
+  var columna = obtenerPosicion("columna");
+  return columna[pos];
+}
 $(function(){
   tituloParpadeo(".main-titulo");
 });
